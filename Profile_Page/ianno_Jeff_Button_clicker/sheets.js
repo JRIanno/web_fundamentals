11ee93d52729eae0a1abd690c9e54f0281d3accb
JRIanno <jianno@gmail.com>
2022-07-17 00:32:21 -0400

    Adding a profile page assignment july 17, 22

---
+++ Profile_Page/ianno_Jeff_Button_clicker/sheets.js
@@ -0,0 +1,11 @@
+let login = document.querySelector('#login');
+
+login.addEventListener('click', function () {
+    login.innerText = 'logout';
+})
+
+let Add = document.querySelector('.Add');
+
+Add.addEventListener('click', function () {
+    Add.style.display = 'none';
+})
